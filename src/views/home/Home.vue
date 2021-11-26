8fa9eae6c9d19f0ad30a65311c7b54918d5a9c2c
passionate <2335275255@qq.com>
2021-11-27 00:15:50 +0800

    '初始化2.0'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,27 @@
 <template>
-
+    <div>
+       <nav-bar class="nav">
+           <div slot="left">
+               <h1>left</h1>
+           </div>
+       </nav-bar>
+    </div>
 </template>
 
 <script>
+    import NavBar from "components/common/navbar/NavBar";
     export default {
-        name: "Home"
+        name: "Home",
+        components:{
+            NavBar
+        }
     }
 </script>
 
 <style scoped>
-
+    .nav{
+        width: 100%;
+        height: 60px;
+        background-color: #cccccc;
+    }
 </style>
